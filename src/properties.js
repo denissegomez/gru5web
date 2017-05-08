bfe9f59ecc131e5e5e256dcd4a4aba3c58433f76
CASAGATUNA <casagatuna@MacBook-Air-de-CASAGATUNA.local>
2017-05-08 23:52:28 +0200

    imágenes

--- src/properties.js
+++ src/properties.js
@@ -1,13 +1,14 @@
 /* Listado inicial de inmuebles disponibles */ 
 var PROPERTIES = [
     {
-        reference: '',
-        address: '',
-        city: '',
-        region: '',
-        numberOfBedrooms: '',
-        price: '',
-        image: '',
+        description: 'Casa moderna en Cumbres de Campestre, completamente amueblada',
+        address: 'calle río luna, 19',
+        city: 'Cumbres de Campestre',
+        region: 'León',
+        year:'2014',
+        numberOfBedrooms: '5',
+        price: '300.000',
+        image: '<img src="cumbcamp.jpg">',
         /* Añadir más propiedades según sean necesarias */
     },
     {
