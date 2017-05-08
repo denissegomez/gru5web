61683a6e7c3c44dcf3fbc57163a921244a375924
Denisse G <Denisse G>
2017-05-08 23:18:36 +0200

    get register form data

--- src/register/register.js
+++ src/register/register.js
@@ -1,5 +1,16 @@
 function register(){
-    var user = { 
+    var user = getUser();
+
+    var validated = validate(user);
+
+    
+    // if validated var result = storageManager.addUser(user);
+
+    // ToDo: if result is false show error message, if true, login the user and redirect.
+}
+
+function getUser(){
+        var user = { 
         // name
         // lastname
         // fullname = name + lastname
@@ -15,15 +26,51 @@ function register(){
         // password
     };
     
-    // TODO read all the pending fields and validate them
-    user.name = document.getElementById("name");
-    user.lastname = document.getElementById("lastname");
+    user.name = document.getElementById("name").value;
+    user.lastname = document.getElementById("lastname").value;
     user.fullname = user.name + " " + user.lastname;
-    user.DNI = document.getElementById("DNI");
-    
-    user.email = document.getElementById("email");    
+    user.DNI = document.getElementById("dni").value;
+    user.birthdate = document.getElementById("birthdate").value;
+    user.address = document.getElementById("address").value;
+    user.city = document.getElementById("city").value;
+    user.zipcode = document.getElementById("zipcode").value;
+    user.phone = document.getElementById("phone").value;
+    user.email = document.getElementById("email").value;
+    user.username  = document.getElementById("username").value;
+    user.password = document.getElementById("password").value;  
 
-    var result = storageManager.addUser(user);
+    return user;   
+}
 
-    // ToDo: if result is false show error message, if true, login the user and redirect.
+function isAdult(birthdate) {
+    var today = new Date();
+    var birth = new Date(birthdate);
+    var age = today.getFullYear() - birth.getFullYear();
+    var month = today.getMonth() - birth.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+        age = age - 1;
+    }
+    return age > 18;
+}
+
+function validate(user){
+    var result = true;
+    var errorMessage = document.getElementById("errorMessage");
+
+    if (isAdult(user.birthdate) == false){
+        errorMessage.innerText = errorMessage.innerText + "\n Debe ser mayor de edad para poder registrarse.";
+        result = false;
+    }
+
+    var verifyPassword = document.getElementById("verifyPassword").value;
+    if (user.password !== verifyPassword){
+        errorMessage.innerText = errorMessage.innerText + "\n No coinciden las contraseñas.";
+        result = false;
+    }
+
+    if (result) {
+        errorMessage.innerText = "";
+    }
+
+    return result;
 }
