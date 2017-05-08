8e14f98368cf506eb3f01817e837ecad44d91a06
CASAGATUNA <casagatuna@MacBook-Air-de-CASAGATUNA.local>
2017-05-09 00:21:28 +0200

    propiedades

--- src/properties.js
+++ src/properties.js
@@ -1,8 +1,8 @@
 /* Listado inicial de inmuebles disponibles */ 
 var PROPERTIES = [
     {
-        description: 'Casa moderna en Cumbres de Campestre, completamente amueblada',
-        address: 'calle río luna, 19',
+        description: 'Casa moderna en Cumbres de Campestre, completamente amueblada, naturaleza de vanguardia',
+        address: 'calle Río Luna, 19',
         city: 'Cumbres de Campestre',
         region: 'León',
         year:'2014',
@@ -13,6 +13,35 @@ var PROPERTIES = [
     },
     {
         /* Añadir aquí el segundo inmueble */
-    }
+    
+        description: 'Casa rural en el Río Cinca, espectaculares vistas y paisajes naturales',
+        address: 'calle Cinca, 19',
+        city: 'Javierre',
+        region: 'Huesca',
+        year:'1990 (reformada en el 2000)',
+        numberOfBedrooms: '6',
+        price: '150.000',
+        image: '<img src="casahuesca.jpg">',
+    },
     /* Añadir más inmuebles según sea necesario */
+    {
+        description: 'Chalet con piscina climatizada, vivienda tranquila a 1 km del pueblo',
+        address: 'calle Rianxeira, 34',
+        city: 'Saians',
+        region: 'Vigo',
+        year:'2013',
+        numberOfBedrooms: '6',
+        price: '355.000',
+        image: '<img src="vigo.jpg">',
+    },
+    {
+        description: 'Piso céntrico, a 100 metros de transporte público, restaurantes y supermercados cerca',
+        address: 'calle Pez, 21',
+        city: 'Madrid',
+        region: 'Madrid',
+        year:'1945 (reformado recientemente)',
+        numberOfBedrooms: '3',
+        price: '244.000',
+        image: '<img src="madrid.jpg">',        
+    }
 ]
