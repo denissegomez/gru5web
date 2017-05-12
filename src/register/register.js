a568dec85d242257f6ccd9fdad3cf9f89c217acf
Denisse G <Denisse G>
2017-05-12 21:11:14 +0200

    Form Styling

--- src/register/register.js
+++ src/register/register.js
@@ -59,43 +59,43 @@ function validate(user){
     errorMessage.innerText = "";
 
     if (isDniValid(user.DNI) == false){
-        errorMessage.innerText = errorMessage.innerText + "\n Debe introducir un DNI válido.";
+        errorMessage.innerText = errorMessage.innerText + "\n - Debe introducir un DNI válido.";
         result = false;
     } 
 
     if (isAdult(user.birthdate) == false){
-        errorMessage.innerText = errorMessage.innerText + "\n Debe ser mayor de edad para poder registrarse.";
+        errorMessage.innerText = errorMessage.innerText + "\n - Debe ser mayor de edad para poder registrarse.";
         result = false;
     }
 
     if (user.zipcode.length !== 5){
-        errorMessage.innerText = errorMessage.innerText + "\n El código postal tiene que tener una longitud de 5 números.";
+        errorMessage.innerText = errorMessage.innerText + "\n - El código postal tiene que tener una longitud de 5 números.";
         result = false;
     }
 
     if (user.phone.length !== 9){
-        errorMessage.innerText = errorMessage.innerText + "\n El teléfono tiene que tener una longitud de 9 números.";
+        errorMessage.innerText = errorMessage.innerText + "\n - El teléfono tiene que tener una longitud de 9 números.";
         result = false;
     }
 
     if (user.password.length < 8){
-        errorMessage.innerText = errorMessage.innerText + "\n La contraseña tiene que tener una longitud de al menos 8 caracteres.";
+        errorMessage.innerText = errorMessage.innerText + "\n - La contraseña tiene que tener una longitud de al menos 8 caracteres.";
         result = false;
     }
 
     if (user.password.replace(/[^A-Z]/g, "").length < 2){
-        errorMessage.innerText = errorMessage.innerText + "\n La contraseña tiene que tener más de una mayúscula.";
+        errorMessage.innerText = errorMessage.innerText + "\n - La contraseña tiene que tener más de una mayúscula.";
         result = false;
     }
 
     if (user.password.replace(/[^0-9]/g, "").length < 2){
-        errorMessage.innerText = errorMessage.innerText + "\n La contraseña tiene que tener más de un número.";
+        errorMessage.innerText = errorMessage.innerText + "\n - La contraseña tiene que tener más de un número.";
         result = false;
     }
 
     var verifyPassword = document.getElementById("verifyPassword").value;
     if (user.password !== verifyPassword){
-        errorMessage.innerText = errorMessage.innerText + "\n No coinciden las contraseñas.";
+        errorMessage.innerText = errorMessage.innerText + "\n - No coinciden las contraseñas.";
         result = false;
     } 
 
