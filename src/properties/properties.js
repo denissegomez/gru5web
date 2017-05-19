caba13777d6674240b17331777df2afa7bd7e024
denissegomez <denissegomezcasco@gmail.com>
2017-05-20 00:18:59 +0200

    Properties filter onchanged

--- src/properties/properties.js
+++ src/properties/properties.js
@@ -2,3 +2,10 @@ function logout(){
     storageManager.logoutUser();
     window.location.href = "../index.html";
 }
+
+function filterChanged(){
+    storageManager.setPropertiesListFilter(document.getElementById('filter').value);
+    
+    var properties = storageManager.getPropertiesBasedOnFilter();
+    // ToDo create the list of cards based on the properties received ordered
+}
